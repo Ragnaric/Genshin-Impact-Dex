8b5ffecc197757aec22b9ea2158b8252261be50e
Jonathan <navakino@gmail.com>
2022-11-17 13:31:28 -0600

    Remove inline height and width from inline style for image gallery

--- client/src/components/ImageGallery.jsx
+++ client/src/components/ImageGallery.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageGallery = ({character}) => {
   return (
     <>
-      { Object.keys(character).length > 0 ? <div className="container z-10 w-full bg-cover bg-no-repeat" style={{backgroundImage: `url(${character.splash_art})`, width: '100%', height: '800px'}}>
+      { Object.keys(character).length > 0 ? <div className="container z-10 w-full bg-cover bg-no-repeat w-full h-[800px]" style={{backgroundImage: `url(${character.splash_art})`}}>
         <div className="m-auto w-1/2 text-center text-5xl text-amber-200">{character.id}</div>
       </div> : null}
     </>
