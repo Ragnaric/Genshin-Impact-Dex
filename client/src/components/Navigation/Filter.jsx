67cfb33992d8eb21c260118ea0bc5151e1111139
Jonathan <navakino@gmail.com>
2022-12-01 13:44:16 -0600

    Refactor filter function to display characters that intersect between filtered lists.

--- client/src/components/Navigation/Filter.jsx
+++ client/src/components/Navigation/Filter.jsx
@@ -2,9 +2,10 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Filter = ({characters, setCharacters, allCharacters}) => {
-  const [isFiltered, setIsFiltered] = useState(false);
-  const [filters, setFilters] = useState([]);
-  const [filteredSet, setFilteredSet] = useState([]);
+  const [elementList, setElementList] = useState([]);
+  const [weaponList, setWeaponList] = useState([]);
+  const [rarityList, setRarityList] = useState([]);
+  const [regionList, setRegionList] = useState([]);
 
   const [isAnemo, setIsAnemo] = useState(false);
   const [isGeo, setIsGeo] = useState(false);
@@ -20,36 +21,67 @@ const Filter = ({characters, setCharacters, allCharacters}) => {
   const [isClaymore, setIsClaymore] = useState(false);
 
   useEffect(() => {
-    console.log('filtered set', filteredSet, 'filters', filters)
-    if (filters.length === 0) {
+    if (elementList.length === 0 && weaponList.length === 0) {
       setCharacters(allCharacters);
     } else {
-      let filteredResults = [];
+      let filteredList = [];
       allCharacters.forEach(character => {
-        for (let i = 0; i < filters.length; i++) {
-          if (filteredResults.indexOf(character) !== -1) {
-            continue;
+        if (elementList.length > 0 && weaponList.length === 0) {
+          setCharacters(elementList);
+        }
+        if (weaponList.length > 0 && elementList.length === 0) {
+          setCharacters(weaponList);
+        }
+        if (elementList.length > 0 && weaponList.length > 0) {
+          if (elementList.indexOf(character) !== -1 && weaponList.indexOf(character) !== -1) {
+            filteredList.push(character);
           }
-          if (character.element === filters[i] || character.weapon === filters[i]) {
-            filteredResults.push(character);
-          }
+          setCharacters(filteredList);
         }
       });
-      setCharacters(filteredResults);
-      setFilteredSet(filteredResults);
     }
-  }, [isFiltered]);
+  }, [elementList, weaponList]);
 
-  const addFilters = (keyword) => {
-    let newFilters = filters.slice();
-    newFilters.push(keyword);
-    setFilters(newFilters);
+  const filterByElement = (keyword) => {
+    let newList = elementList.slice();
+    allCharacters.forEach(character => {
+      if (character.element === keyword) {
+        newList.push(character);
+      }
+    });
+    setElementList(newList);
   };
 
-  const removeFilters = (keyword) => {
-    let newFilters = filters.slice();
-    newFilters.splice(newFilters.indexOf(keyword), 1);
-    setFilters(newFilters);
+  const removeElement = (keyword) => {
+    let newList = elementList.slice();
+    for (let i = 0; i < newList.length; i++) {
+      if (newList[i].element === keyword) {
+        newList.splice(i, 1);
+        i--;
+      }
+    }
+    setElementList(newList);
+  };
+
+  const filterByWeapon = (keyword) => {
+    let newList = weaponList.slice();
+    allCharacters.forEach(character => {
+      if (character.weapon === keyword) {
+        newList.push(character);
+      }
+    })
+    setWeaponList(newList);
+  };
+
+  const removeWeapon = (keyword) => {
+    let newList = weaponList.slice();
+    for (let i = 0; i < newList.length; i++) {
+      if (newList[i].weapon === keyword) {
+        newList.splice(i, 1);
+        i--;
+      }
+    }
+    setWeaponList(newList);
   };
 
   return (
@@ -59,72 +91,65 @@ const Filter = ({characters, setCharacters, allCharacters}) => {
         <div className="inline-block text-white h-20 w-20 bg-no-repeat bg-cover text-center leading-[80px]" style={{backgroundImage: isAnemo ? 'url(https://preview.redd.it/5cgw3tah2ay71.png?width=360&format=png&auto=webp&s=0d7852de9d666068a8b164264d507b16deba27fa)' : 'url(https://static.wikia.nocookie.net/gensin-impact/images/1/10/Element_Anemo.svg)'}} onClick={(event) => {
           if (!isAnemo) {
             setIsAnemo(!isAnemo);
-            addFilters('Anemo');
+            filterByElement('Anemo');
           } else {
             setIsAnemo(!isAnemo);
-            removeFilters('Anemo');
+            removeElement('Anemo');
           }
-          setIsFiltered(!isFiltered);
         }}>Anemo</div>
         <div className="inline-block text-white h-20 w-20 bg-no-repeat bg-cover text-center leading-[80px]" style={{backgroundImage: isGeo ? 'url(https://preview.redd.it/n5ej1aeo2ay71.png?width=360&format=png&auto=webp&s=29b111b484dbf8419ffa628b4d76dd708a1515f2)' : 'url(https://static.wikia.nocookie.net/gensin-impact/images/9/9b/Element_Geo.svg)'}} onClick={(event) => {
           if (!isGeo) {
             setIsGeo(!isGeo);
-            addFilters('Geo');
+            filterByElement('Geo');
           } else {
             setIsGeo(!isGeo);
-            removeFilters('Geo');
+            removeElement('Geo');
           }
-          setIsFiltered(!isFiltered);
         }}>Geo</div>
         <div className="inline-block text-white h-20 w-20 bg-no-repeat bg-cover text-center leading-[80px]" style={{backgroundImage: isPyro ? 'url(https://preview.redd.it/f62yf4o95ay71.png?width=360&format=png&auto=webp&s=a3713c25c9088ca0b9321eed295ec2bca3d0d4b2)' : 'url(https://static.wikia.nocookie.net/gensin-impact/images/2/2c/Element_Pyro.svg)'}} onClick={(event) => {
           if (!isPyro) {
             setIsPyro(!isPyro);
-            addFilters('Pyro');
+            filterByElement('Pyro');
           } else {
             setIsPyro(!isPyro);
-            removeFilters('Pyro');
+            removeElement('Pyro');
           }
-          setIsFiltered(!isFiltered);
         }}>Pyro</div>
         <div className="inline-block text-white h-20 w-20 bg-no-repeat bg-cover text-center leading-[80px]" style={{backgroundImage: isElectro ? 'url(https://preview.redd.it/0ps0mocb3ay71.png?width=360&format=png&auto=webp&s=22b893ac1c5b496b231942bf6b9c54dc22a4a368)' : 'url(https://static.wikia.nocookie.net/gensin-impact/images/f/ff/Element_Electro.svg)'}} onClick={(event) => {
           if (!isElectro) {
             setIsElectro(!isElectro);
-            addFilters('Electro');
+            filterByElement('Electro');
           } else {
             setIsElectro(!isElectro);
-            removeFilters('Electro');
+            removeElement('Electro');
           }
-          setIsFiltered(!isFiltered);
         }}>Electro</div>
         <div className="inline-block text-white h-20 w-20 bg-no-repeat bg-cover text-center leading-[80px]" style={{backgroundImage: isHydro ? 'url(https://preview.redd.it/zwarjvzk4ay71.png?width=360&format=png&auto=webp&s=c755931829096e5a41936c66424905b0454cbb57)' : 'url(https://static.wikia.nocookie.net/gensin-impact/images/8/80/Element_Hydro.svg)'}} onClick={(event) => {
           if (!isHydro) {
             setIsHydro(!isHydro);
-            addFilters('Hydro');
+            filterByElement('Hydro');
           } else {
             setIsHydro(!isHydro);
-            removeFilters('Hydro');
+            removeElement('Hydro');
           }
-          setIsFiltered(!isFiltered);
         }}>Hydro</div>
         <div className="inline-block text-white h-20 w-20 bg-no-repeat bg-cover text-center leading-[80px]" style={{backgroundImage: isCryo ? 'url(https://preview.redd.it/a8kju4zk5ay71.png?width=360&format=png&auto=webp&s=c7483a9a99cd111432e43d823dd85ca2d31891da)' : 'url(https://static.wikia.nocookie.net/gensin-impact/images/7/72/Element_Cryo.svg)'}} onClick={(event) => {
           if (!isCryo) {
             setIsCryo(!isCryo);
-            addFilters('Cryo');
+            filterByElement('Cryo');
           } else {
             setIsCryo(!isCryo);
-            removeFilters('Cryo');
+            removeElement('Cryo');
           }
-          setIsFiltered(!isFiltered);
         }}>Cryo</div>
         <div className="inline-block text-white h-20 w-20 bg-no-repeat bg-contain text-center leading-[80px]" style={{backgroundImage: isDendro ? 'url(https://preview.redd.it/w7jmevct3ay71.png?width=360&format=png&auto=webp&s=5ea427a55c2d573dcddd3bb47e63823fd512df5b)' : 'url(https://static.wikia.nocookie.net/gensin-impact/images/7/73/Element_Dendro.svg)'}} onClick={(event) => {
           if (!isDendro) {
             setIsDendro(!isDendro);
-            addFilters('Dendro');
+            filterByElement('Dendro');
           } else {
             setIsDendro(!isDendro);
-            removeFilters('Dendro');
+            removeElement('Dendro');
           }
-          setIsFiltered(!isFiltered);
         }}>Dendro</div>
       </div>
       <div className='text-amber-200 text-xl text-center m-5'>Filter by Weapon</div>
@@ -132,52 +157,47 @@ const Filter = ({characters, setCharacters, allCharacters}) => {
         <div className="inline-block text-white h-28 w-28 bg-no-repeat bg-cover text-center leading-[100px]" style={{backgroundImage: 'url(https://static.wikia.nocookie.net/gensin-impact/images/8/81/Icon_Bow.png)', borderStyle: isBow ? 'solid' : '', borderWidth: isBow ? '4px' : '', borderRadius: isBow ? '9999px' : ''}} onClick={(event) => {
           if (!isBow) {
             setIsBow(!isBow);
-            addFilters('Bow');
+            filterByWeapon('Bow');
           } else {
             setIsBow(!isBow);
-            removeFilters('Bow');
+            removeWeapon('Bow');
           }
-          setIsFiltered(!isFiltered);
         }}>Bow</div>
         <div className="inline-block text-white h-28 w-28 bg-no-repeat bg-cover text-center leading-[100px]" style={{backgroundImage: 'url(https://static.wikia.nocookie.net/gensin-impact/images/8/81/Icon_Sword.png)', borderStyle: isSword ? 'solid' : '', borderWidth: isSword ? '4px' : '', borderRadius: isSword ? '9999px' : ''}} onClick={(event) => {
           if (!isSword) {
             setIsSword(!isSword);
-            addFilters('Sword');
+            filterByWeapon('Sword');
           } else {
             setIsSword(!isSword);
-            removeFilters('Sword');
+            removeWeapon('Sword');
           }
-          setIsFiltered(!isFiltered);
         }}>Sword</div>
         <div className="inline-block text-white h-28 w-28 bg-no-repeat bg-cover text-center leading-[100px]" style={{backgroundImage: 'url(https://static.wikia.nocookie.net/gensin-impact/images/6/6a/Icon_Polearm.png)', borderStyle: isPolearm ? 'solid' : '', borderWidth: isPolearm ? '4px' : '', borderRadius: isPolearm ? '9999px' : ''}} onClick={(event) => {
           if (!isPolearm) {
             setIsPolearm(!isPolearm);
-            addFilters('Polearm');
+            filterByWeapon('Polearm');
           } else {
             setIsPolearm(!isPolearm);
-            removeFilters('Polearm');
+            removeWeapon('Polearm');
           }
-          setIsFiltered(!isFiltered);
         }}>Polearm</div>
         <div className="inline-block text-white h-28 w-28 bg-no-repeat bg-cover text-center leading-[100px]" style={{backgroundImage: 'url(https://static.wikia.nocookie.net/gensin-impact/images/2/27/Icon_Catalyst.png)', borderStyle: isCatalyst ? 'solid' : '', borderWidth: isCatalyst ? '4px' : '', borderRadius: isCatalyst ? '9999px' : ''}} onClick={(event) => {
           if (!isCatalyst) {
             setIsCatalyst(!isCatalyst);
-            addFilters('Catalyst');
+            filterByWeapon('Catalyst');
           } else {
             setIsCatalyst(!isCatalyst);
-            removeFilters('Catalyst');
+            removeWeapon('Catalyst');
           }
-          setIsFiltered(!isFiltered);
         }}>Catalyst</div>
         <div className="inline-block text-white h-28 w-28 bg-no-repeat bg-cover text-center leading-[100px]" style={{backgroundImage: 'url(https://static.wikia.nocookie.net/gensin-impact/images/6/66/Icon_Claymore.png)', borderStyle: isClaymore ? 'solid' : '', borderWidth: isClaymore ? '4px' : '', borderRadius: isClaymore ? '9999px' : ''}} onClick={(event) => {
           if (!isClaymore) {
             setIsClaymore(!isClaymore);
-            addFilters('Claymore');
+            filterByWeapon('Claymore');
           } else {
             setIsClaymore(!isClaymore);
-            removeFilters('Claymore');
+            removeWeapon('Claymore');
           }
-          setIsFiltered(!isFiltered);
         }}>Claymore</div>
       </div>
     </div>
